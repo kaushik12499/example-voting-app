ae91954b2925fd39cc17cde9cdc5c93bb568c8c8
kaushik12499 <65509329+kaushik12499@users.noreply.github.com>
2021-09-11 12:39:51 +0530

    Delete port.sh

--- port.sh
+++
@@ -1,2 +0,0 @@
-kubectl port-forward --address 0.0.0.0 service/node-result-service 8086:80 2>&1 >/dev/null &
- kubectl port-forward --address 0.0.0.0 service/py-vote-service 8085:80 2>&1 >/dev/null &
