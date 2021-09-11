93b784867a75362986cf806a819789bd12706a31
kaushik12499 <65509329+kaushik12499@users.noreply.github.com>
2021-09-11 16:19:21 +0530

    Update Dockerctrler.sh

--- Dockerctrler.sh
+++ Dockerctrler.sh
@@ -25,8 +25,8 @@ docker logout
 
 #echo " Initializing Containers"
 
-docker run -d --name=redis redis
-docker run -d --name=db -e POSTGRES_HOST_AUTH_METHOD=trust postgres:9.4
-docker run -d --name=vote -p 5000:80 --link redis:redis kaushiktraining/vote-app
-docker run -d --name=result -p 5001:80 --link redis:redis --link db:db kaushiktraining/result-app
-docker run -d --name=worker --link redis:redis --link db:db kaushiktraining/worker-app
+#docker run -d --name=redis redis
+#docker run -d --name=db -e POSTGRES_HOST_AUTH_METHOD=trust postgres:9.4
+#docker run -d --name=vote -p 5000:80 --link redis:redis kaushiktraining/vote-app
+#docker run -d --name=result -p 5001:80 --link redis:redis --link db:db kaushiktraining/result-app
+#docker run -d --name=worker --link redis:redis --link db:db kaushiktraining/worker-app
